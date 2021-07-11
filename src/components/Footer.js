9e48fece6b36a2477550d421eccca238470d1597
Lucas Fernandes <lucas.h.fernandes.reis@gmail.com>
2021-07-11 18:44:28 -0300

    Realiza estilizaçao da pagina de detalhes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import '.Footer.css';
+import './Footer.css';
 
 class Footer extends React.Component {
   render() {
     return (
-      <footer>
-        <h3>
-          Conteúdo de projeto da Trybe® com confecção e adições de Lucas Fernandes!
+      <footer className="footer-content">
+        <h3 className="footer-message">
+          Conteúdo de projeto da Trybe® com confecção e adições de <a className="linkedin" target="_blank" href="https://www.linkedin.com/in/lucasfernandesreis/">Lucas Fernandes</a>
         </h3>
       </footer>
     );
--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
+import './MovieDetails.css';
 
 class MovieDetails extends Component {
   constructor() {
@@ -43,16 +44,23 @@ class MovieDetails extends Component {
       return <Loading />;
     }
     return (
-      <div data-testid="movie-details">
-        <img alt="Movie Cover" src={imagePath} />
-        <h4>{ `Title: ${title}` }</h4>
-        <p>{ `Subtitle: ${subtitle}` }</p>
-        <p>{ `Storyline: ${storyline}` }</p>
-        <p>{ `Genre: ${genre}` }</p>
-        <p>{ `Rating: ${rating}` }</p>
-        <Link to={ `/movies/${title}/edit` }>EDITAR</Link>
-        <Link to="/">VOLTAR</Link>
-        <Link onClick={ () => this.removeMovie(title) } to="/">DELETAR</Link>
+      <div className="details-content" data-testid="movie-details">
+        <div className="img-content">
+          <img className="image-hero" alt="Movie Cover" src={imagePath} />
+          <div className="fade-img"></div>
+        </div>
+        <div className="info-details">
+          <h4 className="title-detail">{title}</h4>
+          <p className="subtitle-detail">{subtitle}</p>
+          <p className="storyline-detail">{ `Storyline: ${storyline}` }</p>
+          <p className="genre-detail">{ `Genre: ${genre}` }</p>
+          <p className="rating-detail">{ `Rating: ${rating}` }</p>
+          <div className="buttons-detail">
+            <Link className="edit-btn" to={ `/movies/${title}/edit` }>EDITAR</Link>
+            <Link className="back-btn" to="/">VOLTAR</Link>
+            <Link className="rm-btn" onClick={ () => this.removeMovie(title) } to="/">DELETAR</Link>
+          </div>
+        </div>
       </div>
     );
   }
