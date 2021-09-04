a7dbdbd797bef2240eac92616d1ed6a1f7da3a4a
Lucas Fernandes <lucas.h.fernandes.reis@gmail.com>
2021-09-03 23:23:27 -0300

    Deploy

---
+++ build/precache-manifest.9847e80104fb5955a35b567a1a355838.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc9ba3c94c0f02b6a922b6f74338509e",
+    "url": "/app-toot-name/index.html"
+  },
+  {
+    "revision": "953fb53aa8d45cf43e03",
+    "url": "/app-toot-name/static/css/main.496ed9bb.chunk.css"
+  },
+  {
+    "revision": "21e3569d47c799fca0c8",
+    "url": "/app-toot-name/static/js/2.701f6da9.chunk.js"
+  },
+  {
+    "revision": "953fb53aa8d45cf43e03",
+    "url": "/app-toot-name/static/js/main.cf44d7bd.chunk.js"
+  },
+  {
+    "revision": "10f00aa2016367073b45",
+    "url": "/app-toot-name/static/js/runtime-main.fb5cccab.js"
+  },
+  {
+    "revision": "b28f07adcbb82bc65d6376ef5ef9bbab",
+    "url": "/app-toot-name/static/media/image.b28f07ad.jpg"
+  },
+  {
+    "revision": "6e55d74b09b9cee4df4e86c66667163e",
+    "url": "/app-toot-name/static/media/moviesCardsWallpaper.6e55d74b.jpg"
+  }
+]);
